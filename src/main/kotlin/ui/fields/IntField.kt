ed92895a7bc1bda9c0a090b0e88b5e7f25cbc341
brndel <ar.braendle@gmail.com>
2024-01-10 14:03:37 +0100

    Add comments to ui Fields

--- src/main/kotlin/ui/fields/IntField.kt
+++ src/main/kotlin/ui/fields/IntField.kt
@@ -2,6 +2,14 @@ package ui.fields
 
 import androidx.compose.runtime.Composable
 
+/**
+ * An input field where the user can select an Int
+ *
+ * @param value the current integer value of the field
+ * @param onValueChange gets called when the user selects a new int
+ * @param maxValue the maximum value that is allowed in this field. If maxValue is `null`, there is no maximum value
+ * @param minValue the minimum value that is allowed in this field. If minValue is `null`, there is not maximum value
+ */
 @Composable
-fun IntField(value: Int, onValueChange: (Int) -> Unit) {
+fun IntField(value: Int, onValueChange: (Int) -> Unit, maxValue: Int?, minValue: Int?) {
 }
--- src/main/kotlin/ui/fields/TextField.kt
+++ src/main/kotlin/ui/fields/TextField.kt
@@ -2,6 +2,12 @@ package ui.fields
 
 import androidx.compose.runtime.Composable
 
+/**
+ * An input field where the user can input a String
+ *
+ * @param value the current string value of this field
+ * @param onValueChange gets called when the user changes the current value
+ */
 @Composable
 fun TextField(value: String, onValueChange: (String) -> Unit) {
 }
