9fb987b68e79c0b982450097a957d0a507dfe43c
brndel <ar.braendle@gmail.com>
2024-01-10 16:06:06 +0100

    Remove App function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,9 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import data.project.Project
 import ui.MainScreen
-
-@Composable
-@Preview
-fun App() {
-    MaterialTheme {
-        MainScreen()
-    }
-}
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        App()
+        MainScreen()
     }
 }
