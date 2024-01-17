5fd45cffce392586a13efe2a92ec58449ff3e9db
brndel <ar.braendle@gmail.com>
2024-01-18 00:19:09 +0100

    Add MaterialTheme composable and provide Language based on system language

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,26 @@
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import ui.Language
+import ui.LocalLanguage
 import ui.MainScreen
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        MainScreen()
+        MaterialTheme {
+            val language = remember {
+                val langCode = System.getProperty("user.language")
+
+                Language.fromCode(langCode)
+            }
+
+            CompositionLocalProvider(
+                LocalLanguage provides language
+            ) {
+                MainScreen()
+            }
+        }
     }
 }
