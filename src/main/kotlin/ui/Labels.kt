21b58949d25cfe9e29547eb58b7a8c195aa728fd
brndel <ar.braendle@gmail.com>
2024-01-18 16:40:13 +0100

    Add SURVIZ Label

--- src/main/kotlin/ui/Labels.kt
+++ src/main/kotlin/ui/Labels.kt
@@ -3,7 +3,8 @@ package ui
 import androidx.compose.runtime.compositionLocalOf
 
 object Labels {
-    const val LAST_PROJECT = "LAST_PROJECT"
+    const val SURVIZ = "SurViz"
+    const val LOAD_LAST_PROJECT = "LAST_PROJECT"
     const val NEW_PROJECT = "NEW_PROJECT"
     const val LOAD_PROJECT = "LOAD_PROJECT"
     const val SETTINGS = "SETTINGS"
@@ -11,13 +12,15 @@ object Labels {
 
 enum class Language(private val code: String, private val strings: Map<String, String>) {
     English("en", mapOf(
-        Labels.LAST_PROJECT to "Last Project",
+        Labels.SURVIZ to "SurViz",
+        Labels.LOAD_LAST_PROJECT to "Load last Project",
         Labels.NEW_PROJECT to "New Project",
         Labels.LOAD_PROJECT to "Load Project",
         Labels.SETTINGS to "Settings",
     )),
     German("de", mapOf(
-        Labels.LAST_PROJECT to "Letztes Projekt",
+        Labels.SURVIZ to "SurViz",
+        Labels.LOAD_LAST_PROJECT to "Letztes Projekt laden",
         Labels.NEW_PROJECT to "Neues Projekt",
         Labels.LOAD_PROJECT to "Projekt laden",
         Labels.SETTINGS to "Einstellungen",
