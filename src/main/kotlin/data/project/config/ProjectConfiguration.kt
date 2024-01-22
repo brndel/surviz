630f1504fae6bcbb2a0599656869314bcc08279f
upyap <upyap@student.kit.edu>
2024-01-22 12:35:07 +0100

    Added init initialization for ProjectConfiguration.kt

--- src/main/kotlin/data/project/config/ProjectConfiguration.kt
+++ src/main/kotlin/data/project/config/ProjectConfiguration.kt
@@ -12,50 +12,54 @@ import java.util.UUID
  * @param singleValueConfigOrder the list of UUID's that stores the order.
  * @param situationConfig the configuration of situations. Each situation is assigned a name.
  */
-class ProjectConfiguration constructor(
-        private var singleValueConfigOrder: SnapshotStateList<UUID>,
-        private var singleValueConfig: SnapshotStateMap<UUID, SingleValueConfig>,
-        private var situationConfig: SnapshotStateMap<String, SituationConfig>
-)
+class ProjectConfiguration {
+    private val singleValueConfigOrder: SnapshotStateList<UUID>
+    private val singleValueConfig: SnapshotStateMap<UUID, SingleValueConfig>
+    private val situationConfig: SnapshotStateMap<String, SituationConfig>
 
-{
-        /**
-         * This method adds a single value to the project.
-         */
-        fun addSingleValue(){
+    init {
+        singleValueConfigOrder = SnapshotStateList<UUID>()
+        singleValueConfig = SnapshotStateMap<UUID, SingleValueConfig>()
+        situationConfig = SnapshotStateMap<String, SituationConfig>()
+    }
 
-        }
+    /**
+     * This method adds a single value to the project.
+     */
+    fun addSingleValue() {
 
-        /**
-         * This method removes the given single value from the project.
-         * @param id the UUID of the single value to be removed
-         */
-        fun removeSingleValue(id: UUID){
+    }
 
-        }
+    /**
+     * This method removes the given single value from the project.
+     * @param id the UUID of the single value to be removed
+     */
+    fun removeSingleValue(id: UUID) {
 
-        /**
-         * This method swaps the order of two given single values.
-         * @param indexA the index of the first single value to be swapped
-         * @param indexB the index of the second single value to be swapped
-         */
-        fun swapSingleValueOrder(indexA: Int, indexB: Int){
+    }
 
-        }
+    /**
+     * This method swaps the order of two given single values.
+     * @param indexA the index of the first single value to be swapped
+     * @param indexB the index of the second single value to be swapped
+     */
+    fun swapSingleValueOrder(indexA: Int, indexB: Int) {
 
-        /**
-         * This method returns the single value configuration.
-         * @return the single value configuration of this project
-         */
-        fun getSingleValues(): SnapshotStateMap<UUID, SingleValueConfig>{
-                return singleValueConfig
-        }
+    }
 
-        /**
-         * This method returns the configuration of situations.
-         * @return the situation configuration of this project
-         */
-        fun getSituationConfig(): SnapshotStateMap<String, SituationConfig>{
-                return situationConfig
-        }
+    /**
+     * This method returns the single value configuration.
+     * @return the single value configuration of this project
+     */
+    fun getSingleValues(): SnapshotStateMap<UUID, SingleValueConfig> {
+        return singleValueConfig
+    }
+
+    /**
+     * This method returns the configuration of situations.
+     * @return the situation configuration of this project
+     */
+    fun getSituationConfig(): SnapshotStateMap<String, SituationConfig> {
+        return situationConfig
+    }
 }
