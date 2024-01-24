332d6c7dff67b7039b060ed74d1da0125f1c3443
Julius Pfister <ursrj@student.kit.edu>
2024-01-24 23:06:36 +0100

    ImageGenerator can print title to image - added test to create situation image - added TextType.kt - added properties for image generator

--- src/main/kotlin/data/io/exporter/ImageGenerator.kt
+++ src/main/kotlin/data/io/exporter/ImageGenerator.kt
@@ -1,10 +1,26 @@
 package data.io.exporter
 
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.TextMeasurer
+import androidx.compose.ui.text.TextPainter
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.createFontFamilyResolver
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.*
+import data.io.exporter.resources.TextType
 import data.project.config.ProjectConfiguration
 import data.project.data.IconStorage
 import data.project.data.Situation
 import data.project.data.SituationOption
 import java.awt.Image
+import java.io.FileInputStream
+import java.util.Locale
+import java.util.Properties
 
 /**
  * This class describes the image generator.
@@ -15,13 +31,26 @@ class ImageGenerator constructor(
     private var config: ProjectConfiguration?,
     private var iconStorage: IconStorage?
 ) {
+
+    private val properties: Properties = Properties()
+
+    init {
+        properties.load(FileInputStream("src/main/resources/config/image_generator.properties"))
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // public functions
+    ///////////////////////////////////////////////////////////////////////////////
+
     /**
      * This method generates a PNG image for the given project configuration.
      * @param config The project configuration.
      * @param iconStorage The icon storage.
      * @return The generated image.
      */
-    fun PngGenerator(config: ProjectConfiguration?, iconStorage: IconStorage?): Image? {
+    fun generatePng(config: ProjectConfiguration, iconStorage: IconStorage?): Image? {
+
+        // warum übergeben wir hier config und icon storage nochmal?
         return null
     }
 
@@ -30,15 +59,73 @@ class ImageGenerator constructor(
      * @param situation The situation.
      * @return The generated image.
      */
-    fun generateSituation(situation: Situation?): Image? {
+    fun generateSituation(situation: Situation): Image? {
         return null
     }
+
     /**
      * This method generates an Image for the given situation option.
      * @param option The situation option.
      * @return The generated image.
      */
-    fun generateOption(option: SituationOption?): Image? {
-        return null
+    fun generateOption(option: SituationOption): ImageBitmap {
+        val width = properties.getProperty("situation_width").toInt()
+        val height = properties.getProperty("situation_height").toInt()
+
+        val image = ImageBitmap(width, height)
+        val canvas = Canvas(image)
+
+        //fill background
+        val backgroundColor = Paint()
+        backgroundColor.color = Color(properties.getValue("background_color").toString().toLong(16))
+        canvas.drawRect(0F, 0F, width.toFloat(), height.toFloat(), backgroundColor)
+
+        //draw option title
+        drawText(
+            canvas,
+            option.name.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() },
+            Color.Black,
+            Offset(properties.getProperty("option_title_x_offset").toFloat(), properties.getProperty("option_title_y_offset").toFloat()),
+            textType = TextType.Title
+        )
+        return image
     }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // private functions
+    ///////////////////////////////////////////////////////////////////////////////
+
+    @OptIn(ExperimentalTextApi::class)
+    private fun drawText(
+        canvas: Canvas,
+        text: String,
+        color: Color,
+        position: Offset,
+        textType: TextType = TextType.Label,
+        width: Int? = null
+    ) {
+        val style = TextStyle(
+            color = color,
+            fontSize = textType.fontSize,
+            fontWeight = textType.fontWeight,
+            textAlign = if (width != null) TextAlign.Center else null
+        )
+
+        val measurer = TextMeasurer(createFontFamilyResolver(), Density(1.0F), LayoutDirection.Ltr)
+
+        val hello = measurer.measure(
+            text,
+            style,
+            constraints = Constraints(
+                minWidth = width ?: 0,
+                maxWidth = width ?: Constraints.Infinity
+            )
+        )
+
+        canvas.save()
+        canvas.translate(position.x, position.y)
+        TextPainter.paint(canvas, hello)
+        canvas.restore()
+    }
+
 }
---
+++ src/test/kotlin/data/io/exporter/ImageGeneratorTest.kt
@@ -0,0 +1,34 @@
+package data.io.exporter
+
+import androidx.compose.ui.graphics.toAwtImage
+import data.project.config.ProjectConfiguration
+import data.project.data.IconStorage
+import data.project.data.SituationOption
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+import java.io.File
+import javax.imageio.ImageIO
+
+class ImageGeneratorTest {
+
+    @Test
+    fun generatePng() {
+        val generator = ImageGenerator(ProjectConfiguration(), IconStorage())
+        generator.generatePng(ProjectConfiguration(), IconStorage())
+    }
+
+    @Test
+    fun generateOptionTest() {
+        val option = SituationOption(
+            "fuss",
+            hashMapOf("fz_fuss" to 10.0)
+        )
+        val generator = ImageGenerator(ProjectConfiguration(), IconStorage())
+        val image = generator.generateOption(option)
+        val bufferedImage = image.toAwtImage()
+        val outputFile = File("src/test/resources/image/test_image.png")
+        ImageIO.write(bufferedImage, "png", outputFile)
+        println(outputFile.absolutePath)
+    }
+}
