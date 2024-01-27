209b920e1bb20b9b69c559a530517f6ee3a4a9e5
brndel <ar.braendle@gmail.com>
2024-01-27 13:36:06 +0100

    Specify a MaterialTheme in Main and make window bigger on startup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,27 @@
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import ui.Language
 import ui.LocalLanguage
 import ui.MainScreen
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        MaterialTheme {
+    val windowState = rememberWindowState(width = 1920.dp, height = 1080.dp, placement = WindowPlacement.Maximized)
+    Window(onCloseRequest = ::exitApplication, state = windowState) {
+        MaterialTheme(
+            colors = lightColors(
+                background = Color(240, 240, 240),
+                surface = Color(230, 230, 230)
+            )
+        ) {
             val language = remember {
                 val langCode = System.getProperty("user.language")
 
