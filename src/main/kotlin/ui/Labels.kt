702294c657049c1f60220245cf53ed9ec78a73db
brndel <ar.braendle@gmail.com>
2024-01-27 13:11:51 +0100

    Added many new labels

--- src/main/kotlin/ui/Labels.kt
+++ src/main/kotlin/ui/Labels.kt
@@ -8,6 +8,15 @@ object Labels {
     const val NEW_PROJECT = "NEW_PROJECT"
     const val LOAD_PROJECT = "LOAD_PROJECT"
     const val SETTINGS = "SETTINGS"
+
+    const val PAGE_SINGLE_VALUE = "PAGE_SINGLE_VALUE"
+    const val PAGE_SITUATION = "PAGE_SITUATION"
+    const val PAGE_EXPORT = "PAGE_EXPORT"
+    const val NEW = "NEW"
+    const val SELECT = "SELECT"
+
+    const val FIELD_UNIT = "FIELD_UNIT"
+    const val FIELD_COLUMN_SCHEME = "FIELD_COLUMN_SCHEME"
 }
 
 enum class Language(private val code: String, private val strings: Map<String, String>) {
@@ -17,6 +26,16 @@ enum class Language(private val code: String, private val strings: Map<String, S
         Labels.NEW_PROJECT to "New Project",
         Labels.LOAD_PROJECT to "Load Project",
         Labels.SETTINGS to "Settings",
+
+        Labels.PAGE_SINGLE_VALUE to "Single Values",
+        Labels.PAGE_SITUATION to "Situations",
+        Labels.PAGE_EXPORT to "Export",
+
+        Labels.NEW to "New",
+        Labels.SELECT to "Select",
+
+        Labels.FIELD_UNIT to "Unit",
+        Labels.FIELD_COLUMN_SCHEME to "Column scheme",
     )),
     German("de", mapOf(
         Labels.SURVIZ to "SurViz",
@@ -24,6 +43,17 @@ enum class Language(private val code: String, private val strings: Map<String, S
         Labels.NEW_PROJECT to "Neues Projekt",
         Labels.LOAD_PROJECT to "Projekt laden",
         Labels.SETTINGS to "Einstellungen",
+
+        Labels.PAGE_SINGLE_VALUE to "Einzelwerte",
+        Labels.PAGE_SITUATION to "Situationen",
+        Labels.PAGE_EXPORT to "Exportieren",
+
+
+        Labels.NEW to "Neu",
+        Labels.SELECT to "Auswählen",
+
+        Labels.FIELD_UNIT to "Einheit",
+        Labels.FIELD_COLUMN_SCHEME to "Spaltenschema",
     ));
 
     fun getString(label: String): String = strings[label] ?: "<MISSING LABEL '$label'>"
