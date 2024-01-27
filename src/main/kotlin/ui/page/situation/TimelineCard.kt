e45f9d7eae2cbecddb48766b2b5e1f3e55250eff
brndel <ar.braendle@gmail.com>
2024-01-27 20:40:23 +0100

    Implement TimelineCard

--- src/main/kotlin/ui/page/situation/TimelineCard.kt
+++ src/main/kotlin/ui/page/situation/TimelineCard.kt
@@ -1,7 +1,23 @@
 package ui.page.situation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import data.project.config.LineType
 import data.project.config.TimelineEntry
+import org.burnoutcrew.reorderable.ReorderableState
+import ui.fields.IconField
+import ui.fields.GenericSelectField
+import ui.util.ReorderHandle
 
 /**
  * In this card the user can edit a [TimelineEntry]
@@ -12,5 +28,32 @@ import data.project.config.TimelineEntry
  * @ui IconField for the icon of the entry
  */
 @Composable
-fun TimelineCard(entry: TimelineEntry, onDelete: () -> Unit) {
+fun TimelineCard(entry: TimelineEntry, onDelete: () -> Unit, columns: List<String>, reorderState: ReorderableState<*>) {
+    var icon by entry.icon
+    var column by entry.column
+    var lineType by entry.lineType
+
+    Surface(
+        color = MaterialTheme.colors.surface,
+        shape = RoundedCornerShape(4.dp)
+    ) {
+
+        Row(Modifier.padding(4.dp), horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+            ReorderHandle(reorderState)
+
+            IconField(icon) { icon = it }
+
+            GenericSelectField(column, { column = it }, options = columns) {
+                Text(it)
+            }
+
+            GenericSelectField(lineType, { lineType = it }, LineType.entries.toList()) {
+                Text(it.toString())
+            }
+
+            IconButton(onDelete) {
+                Icon(Icons.Default.Delete, null)
+            }
+        }
+    }
 }
