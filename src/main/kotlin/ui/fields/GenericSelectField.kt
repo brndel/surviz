2959e1102e9535447ac68d7282c3b669ffba324f
brndel <ar.braendle@gmail.com>
2024-01-27 20:40:07 +0100

    Add GenericSelectField

---
+++ src/main/kotlin/ui/fields/GenericSelectField.kt
@@ -0,0 +1,31 @@
+package ui.fields
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.Button
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+
+@Composable
+fun <T> GenericSelectField(value: T, onValueChange: (T) -> Unit, options: List<T>, item: @Composable (T) -> Unit) {
+    var dropdownExpanded by remember { mutableStateOf(false) }
+
+    Box {
+        Button({ dropdownExpanded = true }) {
+            item(value)
+        }
+
+        DropdownMenu(expanded = dropdownExpanded, onDismissRequest = { dropdownExpanded = false }) {
+            for (option in options) {
+                DropdownMenuItem({
+                    onValueChange(option)
+                    dropdownExpanded = false
+                }) {
+                    item(option)
+                }
+            }
+        }
+
+    }
+}
