54ba4ca382e08252fd93edbdafe27fd73836ac84
brndel <ar.braendle@gmail.com>
2024-01-27 13:33:58 +0100

    Ugly implementation of SingleValueIconCard

--- src/main/kotlin/ui/page/singleValue/SingleValueIconCard.kt
+++ src/main/kotlin/ui/page/singleValue/SingleValueIconCard.kt
@@ -1,7 +1,21 @@
 package ui.page.singleValue
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import data.project.config.SingleValueIcon
+import ui.fields.DoubleField
 import ui.fields.IconField
 
 /**
@@ -15,4 +29,33 @@ import ui.fields.IconField
  */
 @Composable
 fun SingleValueIconCard(icon: SingleValueIcon) {
+    var baseIcon by icon.baseIcon
+
+    Surface(
+        color = MaterialTheme.colors.background,
+        shape = RoundedCornerShape(4.dp),
+    ) {
+        Column(
+            Modifier.padding(4.dp).fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            IconField(
+                baseIcon
+            ) { baseIcon = it }
+
+            for (level in icon.levels) {
+                Row {
+                    IconField(level.icon.value) { level.icon.value = it }
+                    DoubleField(level.lowerThreshold.value, { level.lowerThreshold.value = it })
+                    IconButton({ icon.removeLevel(level) }) {
+                        Icon(Icons.Default.Delete, null)
+                    }
+                }
+            }
+
+            IconButton({ icon.addLevel() }) {
+                Icon(Icons.Default.Add, null)
+            }
+        }
+    }
 }
