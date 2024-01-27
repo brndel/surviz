6d2c28f0d993510dbefa08c646848880e525c582
brndel <ar.braendle@gmail.com>
2024-01-27 20:40:48 +0100

    Implement SituationPage

--- src/main/kotlin/ui/page/situation/SituationPage.kt
+++ src/main/kotlin/ui/page/situation/SituationPage.kt
@@ -1,7 +1,20 @@
 package ui.page.situation
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import data.project.config.ProjectConfiguration
+import data.project.config.SituationConfig
+import data.project.data.DataScheme
+import ui.Label
+import ui.Labels
+import ui.fields.ColorField
 
 /**
  * On this page the user can edit the situations of a [ProjectConfiguration].
@@ -12,6 +25,51 @@ import data.project.config.ProjectConfiguration
  * @ui SituationTab for the currently selected situation
  */
 @Composable
-fun SituationPage(projectConfiguration: ProjectConfiguration) {
+fun SituationPage(projectConfiguration: ProjectConfiguration, dataScheme: DataScheme) {
+    var selectedTab by remember { mutableStateOf(0) }
 
+    Column(Modifier.fillMaxSize().padding(4.dp)) {
+        Label(Labels.PAGE_SITUATION, style = MaterialTheme.typography.h4)
+
+        Surface(
+            Modifier.padding(16.dp).weight(1F),
+            color = MaterialTheme.colors.surface,
+            shape = RoundedCornerShape(4.dp)
+        ) {
+            Column {
+                TabRow(selectedTabIndex = selectedTab, modifier = Modifier.height(42.dp)) {
+                    dataScheme.options.forEachIndexed { index, option ->
+                        Tab(index == selectedTab, onClick = { selectedTab = index }) {
+                            Text(option.name)
+                        }
+                    }
+                }
+                val selectedOption = dataScheme.options[selectedTab]
+
+                val selectedSituation = run {
+                    projectConfiguration.getSituationConfig()
+                        .getOrPut(
+                            selectedOption.name
+                        ) {
+                            SituationConfig(
+                                name = mutableStateOf(""),
+                                color = mutableStateOf(Color.Black),
+                                singleValueColumns = mutableStateMapOf(),
+                                timeline = mutableStateListOf()
+                            )
+                        } // TODO move this to ProjectConfiguration
+                }
+
+                key(selectedTab) {
+                    SituationTab(
+                        selectedSituation,
+                        selectedOption,
+                        projectConfiguration.getSingleValueConfigOrder(),
+                        projectConfiguration.getSingleValues()
+                            .mapValues { it.value.icon.baseIcon.value }, modifier = Modifier.weight(1F)
+                    )
+                }
+            }
+        }
+    }
 }
