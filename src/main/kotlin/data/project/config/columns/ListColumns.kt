a50c2bec49d0e3baa93f72424d5fca606d39bf92
bnicio <benicio@web.de>
2024-01-28 14:16:10 +0100

    Implemented getValue Function and removed unnecessary constructor

--- src/main/kotlin/data/project/config/columns/ListColumns.kt
+++ src/main/kotlin/data/project/config/columns/ListColumns.kt
@@ -1,13 +1,16 @@
 package data.project.config.columns
 
 import data.project.config.SingleValueConfig
+import data.project.config.SituationConfig
 import data.project.data.Situation
+import data.project.data.SituationOption
+import kotlin.math.absoluteValue
 
 /**
  * This class is a type of SingleValueColumn. It contains a list of columns of the Ngene file that were chosen by hand.
  * @param columns the columns that were chosen.
  */
-class ListColumns constructor(
+class ListColumns(
         var columns: List<String>
 ): SingleValueColumn {
     /**
@@ -16,7 +19,21 @@ class ListColumns constructor(
      * @param situation the situation in which the value of the column(s) will be used
      * @return the sum of the column value(s)
      */
-    override fun getValue(singleValueConfig: SingleValueConfig, situation: Situation): Double{
-        return 0.0
+    override fun getValue(
+        singleValueConfig: SingleValueConfig,
+        situationConfig: SituationConfig,
+        situationOption: SituationOption
+    ): Double {
+        var sum = 0.0
+
+        for (key in situationOption.values.keys) {
+            for (columnIdentifier in columns){
+                if (key == columnIdentifier) {
+                    sum += situationOption.values.getValue(key)
+                }
+            }
+        }
+
+        return sum
     }
 }
