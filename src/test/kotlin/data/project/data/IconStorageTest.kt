f1d00d9c861fe9a5134e60741b1d00a192a707be
upyap <upyap@student.kit.edu>
2024-01-31 16:45:06 +0100

    minor changes in IconStorageTest

--- src/test/kotlin/data/project/data/IconStorageTest.kt
+++ src/test/kotlin/data/project/data/IconStorageTest.kt
@@ -1,22 +1,13 @@
 package data.project.data
 
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
-import jdk.javadoc.internal.doclets.formats.html.markup.Text
-import java.awt.Window
-import java.net.URL
-import javax.imageio.ImageIO
+
 
 
 /**
  * Unfertige Testklasse für die Klasse IconStorage
  */
-class IconStorageTest
+class IconStorageTest {
+
+}
 
