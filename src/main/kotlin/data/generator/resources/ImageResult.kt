8b58e12dc5429c53557774e8ef9a1a96b9d045a9
Julius Pfister <ursrj@student.kit.edu>
2024-01-31 23:41:04 +0100

    added checkWidth() to ImageResult

--- src/main/kotlin/data/generator/resources/ImageResult.kt
+++ src/main/kotlin/data/generator/resources/ImageResult.kt
@@ -2,4 +2,14 @@ package data.generator.resources
 
 import androidx.compose.ui.graphics.ImageBitmap
 
-data class ImageResult(val image: ImageBitmap, val neededWidth: Int)
+data class ImageResult(val image: ImageBitmap, val neededWidth: Int) {
+
+    /**
+     * Check if all of the contents fit on the image
+     *
+     * @return if image is wide enough
+     */
+    fun checkWidth(): Boolean{
+        return neededWidth <= image.width
+    }
+}
