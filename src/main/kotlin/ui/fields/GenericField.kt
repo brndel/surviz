ced65429ec94edcd1b36b07244a1eee2a11dbccd
brndel <ar.braendle@gmail.com>
2024-02-02 22:50:30 +0100

    Implement placeholder for FileSchemeFieldData

--- src/main/kotlin/ui/fields/GenericField.kt
+++ src/main/kotlin/ui/fields/GenericField.kt
@@ -1,5 +1,6 @@
 package ui.fields
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,7 +19,7 @@ fun GenericField(field: FieldData, modifier: Modifier = Modifier) {
     when (field) {
         is BooleanFieldData -> BooleanField(field.value.value, { field.value.value = it }, modifier) { Label(field.getLabel()) }
         is ColorFieldData -> ColorField(field.value.value, { field.value.value = it }, modifier) { Label(field.getLabel()) }
-        is FileSchemeFieldData -> Text("File - TODO")
+        is FileSchemeFieldData -> TextField(field.value.value, { field.value.value = it }, modifier) { Row { Label(field.getLabel()); Text("THIS IS A NORMAL TEXT FIELD FOR NOW") } }
         is IntFieldData -> IntField(
             field.value.value,
             { field.value.value = it },
