9eaa9d8621e57c4c61c74cc54d1a0b051158b839
brndel <ar.braendle@gmail.com>
2024-02-07 20:28:35 +0100

    Make OptionsField look like other fields

--- src/main/kotlin/ui/fields/OptionsField.kt
+++ src/main/kotlin/ui/fields/OptionsField.kt
@@ -2,34 +2,36 @@ package ui.fields
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Button
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun <T> OptionsField(
     value: T,
     onValueChange: (T) -> Unit,
     options: List<T>,
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier.width(128.dp),
     label: @Composable (() -> Unit)? = null,
     item: @Composable (T) -> Unit
 ) {
     var dropdownExpanded by remember { mutableStateOf(false) }
 
-    Row(modifier, verticalAlignment = Alignment.CenterVertically) {
-        label?.invoke()
-
+    OutlinedTextField(value.toString(), {}, modifier, readOnly = true, label = label, trailingIcon = {
         Box {
-            Button({ dropdownExpanded = true }) {
-                item(value)
+            IconButton({
+                dropdownExpanded = !dropdownExpanded
+            }) {
+                Icon(Icons.Default.KeyboardArrowDown, null)
             }
 
             DropdownMenu(
@@ -45,7 +47,5 @@ fun <T> OptionsField(
                 }
             }
         }
-    }
-
-
+    })
 }
