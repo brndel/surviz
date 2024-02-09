03db2032506e1324a9173a45424b307eb1bf7d29
Julius Pfister <ursrj@student.kit.edu>
2024-02-09 14:20:57 +0100

    refactored project path picker

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -21,6 +21,7 @@ import data.project.Project
 import data.project.ProjectData
 import ui.*
 import ui.fields.DirectoryPickerField
+import ui.window.save.SaveProjectWindow
 import ui.window.settings.SettingsWindow
 import java.io.File
 import java.nio.file.Path
@@ -113,7 +114,7 @@ fun main() = application {
             }
 
             if (filePickerOpen) {
-                ProjectPathPicker({ filePickerOpen = false }) {
+                SaveProjectWindow({ filePickerOpen = false }) {
                     currentProjectPath = it.pathString
                     callbacks.saveProject()
                 }
@@ -157,54 +158,6 @@ fun ApplicationScope.MainWindow(
         onKeyEvent = { onKeyEvent(it) }) {
 
         MainScreen(currentProject)
-    }
-}
-
-@Composable
-fun ProjectPathPicker(
-    onCloseRequest: () -> Unit,
-    onFilePicked: (Path) -> Unit
-) {
-    var directory: String by remember { mutableStateOf(Project.defaultSaveDirectory) }
-
-    var filename: String by remember { mutableStateOf(Project.DEFAULT_FILE_NAME) }
-
-    val path by derivedStateOf {
-        var p = Path(directory, filename)
-        if (p.extension == "") {
-            p = Path(p.pathString.removeSuffix(".") + ".svz")
-        }
-
-        p
-    }
-
-    DialogWindow(
-        onCloseRequest = onCloseRequest,
-        icon = rememberVectorPainter(Icons.Default.Save),
-        title = LocalLanguage.current.getString(Labels.ACTION_SAVE_AS)
-    ) {
-        Column(
-            Modifier.padding(10.dp),
-            verticalArrangement = Arrangement.spacedBy(10.dp)
-        ) {
-            DirectoryPickerField(directory, { directory = it }, label = {Label(Labels.ACTION_SAVE_AS_DIRECTORY)})
-
-            OutlinedTextField(filename, { filename = it }, label = {Label(Labels.ACTION_SAVE_AS_PROJECT_NAME)})
-            Text(path.pathString)
-
-            Button({
-                onCloseRequest()
-                onFilePicked(path)
-            }) {
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(10.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Label(Labels.ACTION_SAVE)
-                    Icon(Icons.Default.Save, contentDescription = null)
-                }
-            }
-        }
     }
 }
 
