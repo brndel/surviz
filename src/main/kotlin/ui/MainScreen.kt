e4df637c3cc3b0c7634ac4fec489b4e66f16beda
brndel <ar.braendle@gmail.com>
2024-02-13 22:41:50 +0100

    Cleanup window resize on project load/close

--- src/main/kotlin/ui/MainScreen.kt
+++ src/main/kotlin/ui/MainScreen.kt
@@ -1,16 +1,8 @@
 package ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.WindowPlacement
-import androidx.compose.ui.window.WindowPosition
-import androidx.compose.ui.window.WindowState
-import androidx.compose.ui.window.rememberWindowState
 import data.project.Project
 
 /**
@@ -23,21 +15,12 @@ import data.project.Project
  * @ui ProjectScreen is visible when a valid project is loaded
  */
 @Composable
-fun MainScreen(project: Project?, setWindowState: (WindowState) -> Unit) {
+fun MainScreen(project: Project?) {
     Surface(color = MaterialTheme.colors.background) {
         if (project == null) {
             WelcomeScreen()
-            setWindowState(
-                rememberWindowState(
-                    width = 700.dp,
-                    height = 500.dp,
-                    placement = WindowPlacement.Floating,
-                    position = WindowPosition((1920.dp - 700.dp) / 2, (1080.dp - 500.dp) / 2)
-                )
-            )
         } else {
             ProjectScreen(project)
-            setWindowState(rememberWindowState(placement = WindowPlacement.Maximized))
         }
     }
 }
