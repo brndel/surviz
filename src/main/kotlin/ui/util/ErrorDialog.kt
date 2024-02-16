05234cd2a821c272606beb454ebe118cecc08c21
Julius Pfister <ursrj@student.kit.edu>
2024-02-16 18:10:29 +0100

    added icon to ErrorDialog and slightly changed layout

--- src/main/kotlin/ui/util/ErrorDialog.kt
+++ src/main/kotlin/ui/util/ErrorDialog.kt
@@ -6,9 +6,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Error
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogWindow
 import ui.Label
 import ui.Labels
@@ -17,7 +22,13 @@ import ui.LocalLanguage
 @Composable
 fun ErrorDialog(label: String?, onCloseRequest: () -> Unit) {
     label?.let {
-        DialogWindow(onCloseRequest = onCloseRequest, title = LocalLanguage.current.getString(Labels.ERROR)) {
+        DialogWindow(
+            onCloseRequest = onCloseRequest,
+            title = LocalLanguage.current.getString(Labels.ERROR),
+            icon = rememberVectorPainter(
+                Icons.Default.Error
+            )
+        ) {
             Surface(
                 color = MaterialTheme.colors.background,
                 modifier = Modifier.fillMaxSize()
@@ -26,9 +37,14 @@ fun ErrorDialog(label: String?, onCloseRequest: () -> Unit) {
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Label(it)
-                    Button(onCloseRequest) {
-                        Label(Labels.OK)
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(10.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Label(it)
+                        Button(onCloseRequest) {
+                            Label(Labels.OK)
+                        }
                     }
                 }
             }
