bb61691c788802c4a004a85bad59626d672f5003
Julius Pfister <ursrj@student.kit.edu>
2024-10-11 12:23:57 +0200

    add test file for csv-import

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Exe, TargetFormat.Deb)
             packageName = "SurViz"
-            packageVersion = "1.0.2"
+            packageVersion = "1.0.3"
             vendor = "com.surviz"
             modules("java.compiler", "java.instrument" , "java.sql", "jdk.unsupported")
 
---
+++ src/main/kotlin/data/io/importer/CsvImporter.kt
@@ -0,0 +1,14 @@
+package data.io.importer
+
+import data.project.ProjectData
+import java.io.File
+
+object CsvImporter : Importer {
+    override fun importFile(file: File): ProjectData {
+        TODO("Not yet implemented")
+    }
+
+    override fun getFileExtension(): String {
+        TODO("Not yet implemented")
+    }
+}
